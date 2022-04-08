11d6be8e1e8f10f8d0bfee0c37798b89e14392e1
Neeraj Kashyap <nkashy1@gmail.com>
2022-04-08 09:25:03 -0700

    [WIP] Tests for ERC721 claims

--- lootbox/test_dropper.py
+++ lootbox/test_dropper.py
@@ -310,7 +310,7 @@ class DropperWithdrawalTests(DropperTestCase):
         self.assertEqual(balance_attacker_1, balance_attacker_0)
 
 
-class DropperClaimTests(DropperTestCase):
+class DropperClaimERC20Tests(DropperTestCase):
     def test_claim_erc20(self):
         reward = 3
         self.erc20_contract.mint(self.dropper.address, 100, {"from": accounts[0]})
@@ -534,3 +534,219 @@ class DropperClaimTests(DropperTestCase):
 
         self.assertEqual(balance_claimant_2, balance_claimant_1)
         self.assertEqual(balance_dropper_2, balance_dropper_1)
+
+
+class DropperClaimERC721Tests(DropperTestCase):
+    def test_claim_erc721(self):
+        token_id = 103
+        self.nft_contract.mint(self.dropper.address, token_id, {"from": accounts[0]})
+        claim_id = self.create_claim_and_return_claim_id(
+            721, self.nft_contract.address, token_id, 1, {"from": accounts[0]}
+        )
+        self.dropper.set_signer_for_claim(
+            claim_id, self.signer_0.address, {"from": accounts[0]}
+        )
+
+        current_block = len(chain)
+        block_deadline = current_block  # since blocks are 0-indexed
+
+        message_hash = self.dropper.claim_message_hash(
+            claim_id, accounts[1].address, block_deadline
+        )
+        signed_message = sign_message(message_hash, self.signer_0)
+
+        self.assertEqual(self.nft_contract.owner_of(token_id), self.dropper.address)
+
+        self.dropper.claim(
+            claim_id, block_deadline, signed_message, {"from": accounts[1]}
+        )
+
+        self.assertEqual(self.nft_contract.owner_of(token_id), accounts[1].address)
+
+    def test_claim_erc721_fails_if_block_deadline_exceeded(self):
+        token_id = 105
+        self.nft_contract.mint(self.dropper.address, token_id, {"from": accounts[0]})
+        claim_id = self.create_claim_and_return_claim_id(
+            721, self.nft_contract.address, token_id, 1, {"from": accounts[0]}
+        )
+        self.dropper.set_signer_for_claim(
+            claim_id, self.signer_0.address, {"from": accounts[0]}
+        )
+
+        current_block = len(chain)
+        block_deadline = current_block - 1
+
+        message_hash = self.dropper.claim_message_hash(
+            claim_id, accounts[1].address, block_deadline
+        )
+        signed_message = sign_message(message_hash, self.signer_0)
+
+        self.assertEqual(self.nft_contract.owner_of(token_id), self.dropper.address)
+
+        with self.assertRaises(VirtualMachineError):
+            self.dropper.claim(
+                claim_id, block_deadline, signed_message, {"from": accounts[1]}
+            )
+
+        self.assertEqual(self.nft_contract.owner_of(token_id), self.dropper.address)
+
+    # def test_claim_erc721_fails_if_wrong_claimant(self):
+    #     reward = 6
+    #     self.nft_contract.mint(self.dropper.address, 100, {"from": accounts[0]})
+    #     claim_id = self.create_claim_and_return_claim_id(
+    #         721, self.nft_contract.address, 0, reward, {"from": accounts[0]}
+    #     )
+    #     self.dropper.set_signer_for_claim(
+    #         claim_id, self.signer_0.address, {"from": accounts[0]}
+    #     )
+
+    #     current_block = len(chain)
+    #     block_deadline = current_block  # since blocks are 0-indexed
+
+    #     message_hash = self.dropper.claim_message_hash(
+    #         claim_id, accounts[1].address, block_deadline
+    #     )
+    #     signed_message = sign_message(message_hash, self.signer_0)
+
+    #     balance_attacker_0 = self.nft_contract.balance_of(accounts[2].address)
+    #     balance_claimant_0 = self.nft_contract.balance_of(accounts[1].address)
+    #     balance_dropper_0 = self.nft_contract.balance_of(self.dropper.address)
+
+    #     with self.assertRaises(VirtualMachineError):
+    #         self.dropper.claim(
+    #             claim_id, block_deadline, signed_message, {"from": accounts[2]}
+    #         )
+
+    #     balance_attacker_1 = self.nft_contract.balance_of(accounts[2].address)
+    #     balance_claimant_1 = self.nft_contract.balance_of(accounts[1].address)
+    #     balance_dropper_1 = self.nft_contract.balance_of(self.dropper.address)
+
+    #     self.assertEqual(balance_attacker_1, balance_attacker_0)
+    #     self.assertEqual(balance_claimant_1, balance_claimant_0)
+    #     self.assertEqual(balance_dropper_1, balance_dropper_0)
+
+    # def test_claim_erc721_fails_if_wrong_signer(self):
+    #     reward = 7
+    #     self.nft_contract.mint(self.dropper.address, 100, {"from": accounts[0]})
+    #     claim_id = self.create_claim_and_return_claim_id(
+    #         721, self.nft_contract.address, 0, reward, {"from": accounts[0]}
+    #     )
+    #     self.dropper.set_signer_for_claim(
+    #         claim_id, self.signer_0.address, {"from": accounts[0]}
+    #     )
+
+    #     current_block = len(chain)
+    #     block_deadline = current_block  # since blocks are 0-indexed
+
+    #     message_hash = self.dropper.claim_message_hash(
+    #         claim_id, accounts[1].address, block_deadline
+    #     )
+    #     signed_message = sign_message(message_hash, self.signer_1)
+
+    #     balance_claimant_0 = self.nft_contract.balance_of(accounts[1].address)
+    #     balance_dropper_0 = self.nft_contract.balance_of(self.dropper.address)
+
+    #     with self.assertRaises(VirtualMachineError):
+    #         self.dropper.claim(
+    #             claim_id, block_deadline, signed_message, {"from": accounts[2]}
+    #         )
+
+    #     balance_claimant_1 = self.nft_contract.balance_of(accounts[1].address)
+    #     balance_dropper_1 = self.nft_contract.balance_of(self.dropper.address)
+
+    #     self.assertEqual(balance_claimant_1, balance_claimant_0)
+    #     self.assertEqual(balance_dropper_1, balance_dropper_0)
+
+    # def test_claim_erc721_fails_on_repeated_attempts_with_same_signed_message(self):
+    #     reward = 9
+    #     self.nft_contract.mint(self.dropper.address, 100, {"from": accounts[0]})
+    #     claim_id = self.create_claim_and_return_claim_id(
+    #         721, self.nft_contract.address, 0, reward, {"from": accounts[0]}
+    #     )
+    #     self.dropper.set_signer_for_claim(
+    #         claim_id, self.signer_0.address, {"from": accounts[0]}
+    #     )
+
+    #     current_block = len(chain)
+    #     block_deadline = current_block + 1000
+
+    #     message_hash = self.dropper.claim_message_hash(
+    #         claim_id, accounts[1].address, block_deadline
+    #     )
+    #     signed_message = sign_message(message_hash, self.signer_0)
+
+    #     balance_claimant_0 = self.nft_contract.balance_of(accounts[1].address)
+    #     balance_dropper_0 = self.nft_contract.balance_of(self.dropper.address)
+
+    #     self.dropper.claim(
+    #         claim_id, block_deadline, signed_message, {"from": accounts[1]}
+    #     )
+
+    #     balance_claimant_1 = self.nft_contract.balance_of(accounts[1].address)
+    #     balance_dropper_1 = self.nft_contract.balance_of(self.dropper.address)
+
+    #     self.assertEqual(balance_claimant_1, balance_claimant_0 + reward)
+    #     self.assertEqual(balance_dropper_1, balance_dropper_0 - reward)
+
+    #     with self.assertRaises(VirtualMachineError):
+    #         self.dropper.claim(
+    #             claim_id, block_deadline, signed_message, {"from": accounts[1]}
+    #         )
+
+    #     balance_claimant_2 = self.nft_contract.balance_of(accounts[1].address)
+    #     balance_dropper_2 = self.nft_contract.balance_of(self.dropper.address)
+
+    #     self.assertEqual(balance_claimant_2, balance_claimant_1)
+    #     self.assertEqual(balance_dropper_2, balance_dropper_1)
+
+    # def test_claim_erc721_fails_on_repeated_attempts_with_different_signed_messages(
+    #     self,
+    # ):
+    #     reward = 9
+    #     self.nft_contract.mint(self.dropper.address, 100, {"from": accounts[0]})
+    #     claim_id = self.create_claim_and_return_claim_id(
+    #         721, self.nft_contract.address, 0, reward, {"from": accounts[0]}
+    #     )
+    #     self.dropper.set_signer_for_claim(
+    #         claim_id, self.signer_0.address, {"from": accounts[0]}
+    #     )
+
+    #     current_block = len(chain)
+    #     block_deadline = current_block
+
+    #     message_hash = self.dropper.claim_message_hash(
+    #         claim_id, accounts[1].address, block_deadline
+    #     )
+    #     signed_message = sign_message(message_hash, self.signer_0)
+
+    #     balance_claimant_0 = self.nft_contract.balance_of(accounts[1].address)
+    #     balance_dropper_0 = self.nft_contract.balance_of(self.dropper.address)
+
+    #     self.dropper.claim(
+    #         claim_id, block_deadline, signed_message, {"from": accounts[1]}
+    #     )
+
+    #     balance_claimant_1 = self.nft_contract.balance_of(accounts[1].address)
+    #     balance_dropper_1 = self.nft_contract.balance_of(self.dropper.address)
+
+    #     self.assertEqual(balance_claimant_1, balance_claimant_0 + reward)
+    #     self.assertEqual(balance_dropper_1, balance_dropper_0 - reward)
+
+    #     current_block = len(chain)
+    #     block_deadline = current_block
+
+    #     message_hash = self.dropper.claim_message_hash(
+    #         claim_id, accounts[1].address, block_deadline
+    #     )
+    #     signed_message = sign_message(message_hash, self.signer_0)
+
+    #     with self.assertRaises(VirtualMachineError):
+    #         self.dropper.claim(
+    #             claim_id, block_deadline, signed_message, {"from": accounts[1]}
+    #         )
+
+    #     balance_claimant_2 = self.nft_contract.balance_of(accounts[1].address)
+    #     balance_dropper_2 = self.nft_contract.balance_of(self.dropper.address)
+
+    #     self.assertEqual(balance_claimant_2, balance_claimant_1)
+    #     self.assertEqual(balance_dropper_2, balance_dropper_1)
