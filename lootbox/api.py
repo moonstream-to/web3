51555c5ab4e49ac80708073dc75d257a90625d94
Neeraj Kashyap <nkashy1@gmail.com>
2022-04-12 06:09:54 -0700

    WIP - decided to use journals instead of resources

--- lootbox/api.py
+++ lootbox/api.py
@@ -2,17 +2,22 @@
 Lootbox API.
 """
 import logging
-from typing import Dict
+from typing import Dict, List
+import uuid
 
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from . import data
 from .middleware import BroodAuthMiddleware
 from .settings import (
+    MOONSTREAM_ENGINE_APPLICATION_ID,
+    bugout_client as bc,
     DOCS_TARGET_PATH,
     ORIGINS,
 )
+
+RESOURCE_TYPE_DROP_WHITELIST = "drop_whitelist"
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -51,13 +56,33 @@ async def ping_handler() -> data.PingResponse:
     return data.PingResponse(status="ok")
 
 
-# POST /drops - upload CSV file and register a claim for that CSV file on Dropper contract at a given address
+# POST /drops - add an address for a drop
+# DELETE /drops - remove an address from a drop
 # GET /drops/<dropper_address>/<claim_id>/<address> - get signed transaction for user with the given address [SIGNATURE WORKFLOW]
 # GET /drops?dropper_address=<dropper_address>&claim_id=<claim_id>&address=<address> - list all drops with the given filters
 
+# TODO: Do this with journals instead of resources!!!
+
 
 @app.post("/drops/")
-async def create_drop(register_request: data.DropRegisterRequest = Body(...)) -> None:
-    print("Dropper address:", register_request.dropper_address)
-    print("Claim ID:", register_request.claim_id)
-    print("Addresses:", register_request.addresses)
+async def create_drop(
+    request: Request, register_request: data.DropRegisterRequest = Body(...)
+) -> List[str]:
+    user = request.state.user
+    # TODO(zomglings): Check if user has permission to add to whitelist for drop.
+    bugout_access_token = request.state.token
+    resource_ids: List[str] = []
+    for address in register_request.addresses:
+        resource = bc.resource.create_resource(
+            bugout_access_token,
+            MOONSTREAM_ENGINE_APPLICATION_ID,
+            {
+                "type": RESOURCE_TYPE_DROP_WHITELIST,
+                "dropper_address": register_request.dropper_address,
+                "claim_id": register_request.claim_id,
+                "address": address,
+            },
+        )
+        resource_ids.append(str(resource.id))
+
+    return resource_ids
