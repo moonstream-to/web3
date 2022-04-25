4a27346be2b18b7b24f18ba29da1494e7751eccd
Andrey Dolgolev <andrey@simiotics.com>
2022-04-25 22:16:58 +0300

    Fix api.

--- lootbox/api.py
+++ lootbox/api.py
@@ -153,145 +153,146 @@ async def get_drop_list_handler(
 
 # todo: Enable this endpoints after we add proper authorization
 
-# @app.post("/drops/claims", response_model=data.DropCreatedResponse)
-# async def create_drop(
-#     register_request: data.DropRegisterRequest = Body(...),
-#     db_session: Session = Depends(db.yield_db_session),
-# ) -> data.DropCreatedResponse:
 
-#     """
-#     Create a drop for a given dropper contract.
-#     required: Web3 verification of signature (middleware probably)
-#     body:
-#         dropper_contract_address: address of dropper contract
-#         claim_id: claim id
-#         address: address of claimant
-#         amount: amount of drop
+@app.post("/drops/claims", response_model=data.DropCreatedResponse)
+async def create_drop(
+    register_request: data.DropRegisterRequest = Body(...),
+    db_session: Session = Depends(db.yield_db_session),
+) -> data.DropCreatedResponse:
 
-#     """
+    """
+    Create a drop for a given dropper contract.
+    required: Web3 verification of signature (middleware probably)
+    body:
+        dropper_contract_address: address of dropper contract
+        claim_id: claim id
+        address: address of claimant
+        amount: amount of drop
 
-#     if register_request.terminus_address is None:
-#         register_request.terminus_address = web3.toChecksumAddress(
-#             "0x0000000000000000000000000000000000000000"
-#         )
-#     else:
-#         register_request.terminus_address = web3.toChecksumAddress(
-#             register_request.terminus_address
-#         )
+    """
 
-#     if register_request.terminus_pool_id is None:
-#         register_request.terminus_pool_id = 0
+    if register_request.terminus_address is None:
+        register_request.terminus_address = web3.toChecksumAddress(
+            "0x0000000000000000000000000000000000000000"
+        )
+    else:
+        register_request.terminus_address = web3.toChecksumAddress(
+            register_request.terminus_address
+        )
 
-#     try:
-#         claim = actions.create_claim(
-#             db_session=db_session,
-#             dropper_contract_id=register_request.dropper_contract_id,
-#             title=register_request.title,
-#             description=register_request.description,
-#             claim_block_deadline=register_request.claim_block_deadline,
-#             terminus_address=register_request.terminus_address,
-#             terminus_pool_id=register_request.terminus_pool_id,
-#             claim_id=register_request.claim_id,
-#         )
-#     except NoResultFound:
-#         raise DropperHTTPException(status_code=404, detail="Dropper contract not found")
-#     except Exception as e:
-#         raise DropperHTTPException(status_code=500, detail=str(e))
+    if register_request.terminus_pool_id is None:
+        register_request.terminus_pool_id = 0
 
-#     return data.DropCreatedResponse(
-#         dropper_claim_id=claim.id,
-#         dropper_contract_id=claim.dropper_contract_id,
-#         title=claim.title,
-#         description=claim.description,
-#         claim_block_deadline=claim.claim_block_deadline,
-#         terminus_address=claim.terminus_address,
-#         terminus_pool_id=claim.terminus_pool_id,
-#         claim_id=claim.claim_id,
-#     )
+    try:
+        claim = actions.create_claim(
+            db_session=db_session,
+            dropper_contract_id=register_request.dropper_contract_id,
+            title=register_request.title,
+            description=register_request.description,
+            claim_block_deadline=register_request.claim_block_deadline,
+            terminus_address=register_request.terminus_address,
+            terminus_pool_id=register_request.terminus_pool_id,
+            claim_id=register_request.claim_id,
+        )
+    except NoResultFound:
+        raise DropperHTTPException(status_code=404, detail="Dropper contract not found")
+    except Exception as e:
+        raise DropperHTTPException(status_code=500, detail=str(e))
 
-
-# @app.get("/drops/claimants", response_model=data.DropListResponse)
-# async def get_claimants(
-#     dropper_claim_id: UUID,
-#     limit: int = 10,
-#     offset: int = 0,
-#     db_session: Session = Depends(db.yield_db_session),
-# ) -> List[str]:
-#     """
-#     Get list of claimants for a given dropper contract.
-
-#     curl -X GET "http://localhost:8000/drops/claimants?claim_id=<claim_number>"
-
-#     """
-#     try:
-#         results = actions.get_claimants(
-#             db_session=db_session,
-#             dropper_claim_id=dropper_claim_id,
-#             limit=limit,
-#             offset=offset,
-#         )
-#     except Exception as e:
-#         raise DropperHTTPException(status_code=500, detail=str(e))
-
-#     return data.DropListResponse(drops=list(results))
+    return data.DropCreatedResponse(
+        dropper_claim_id=claim.id,
+        dropper_contract_id=claim.dropper_contract_id,
+        title=claim.title,
+        description=claim.description,
+        claim_block_deadline=claim.claim_block_deadline,
+        terminus_address=claim.terminus_address,
+        terminus_pool_id=claim.terminus_pool_id,
+        claim_id=claim.claim_id,
+    )
 
 
-# @app.post("/drops/claimants", response_model=data.ClaimantsResponse)
-# async def create_claimants(
-#     request: Request,
-#     add_claimants_request: data.DropAddClaimantsRequest = Body(...),
-#     db_session: Session = Depends(db.yield_db_session),
-# ) -> data.ClaimantsResponse:
+@app.get("/drops/claimants", response_model=data.DropListResponse)
+async def get_claimants(
+    dropper_claim_id: UUID,
+    limit: int = 10,
+    offset: int = 0,
+    db_session: Session = Depends(db.yield_db_session),
+) -> List[str]:
+    """
+    Get list of claimants for a given dropper contract.
 
-#     """
-#     Add addresses to particular claim
+    curl -X GET "http://localhost:8000/drops/claimants?claim_id=<claim_number>"
 
-#     curl --location --request POST 'localhost:7191/drops/claimants' \
-#     --header 'Content-Type: application/json' \
-#     --data-raw '{"dropper_claim_id": UUID, "claimants":[{"address": address, "amount": int}]}'
+    """
+    try:
+        results = actions.get_claimants(
+            db_session=db_session,
+            dropper_claim_id=dropper_claim_id,
+            limit=limit,
+            offset=offset,
+        )
+    except Exception as e:
+        raise DropperHTTPException(status_code=500, detail=str(e))
 
-#     """
-
-#     added_by = "me"  # request.state.user.address read from header in auth middleware
-
-#     try:
-#         results = actions.add_claimants(
-#             db_session=db_session,
-#             dropper_claim_id=add_claimants_request.dropper_claim_id,
-#             claimants=add_claimants_request.claimants,
-#             added_by=added_by,
-#         )
-#     except Exception as e:
-#         raise DropperHTTPException(
-#             status_code=500, detail=f"Error adding claimants {e}"
-#         )
-
-#     return data.ClaimantsResponse(claimants=results)
+    return data.DropListResponse(drops=list(results))
 
 
-# @app.delete("/drops/claimants", response_model=data.RemoveClaimantsResponse)
-# async def create_claimants(
-#     request: Request,
-#     remove_claimants_request: data.DropRemoveClaimantsRequest = Body(...),
-#     db_session: Session = Depends(db.yield_db_session),
-# ) -> data.RemoveClaimantsResponse:
+@app.post("/drops/claimants", response_model=data.ClaimantsResponse)
+async def create_claimants(
+    request: Request,
+    add_claimants_request: data.DropAddClaimantsRequest = Body(...),
+    db_session: Session = Depends(db.yield_db_session),
+) -> data.ClaimantsResponse:
 
-#     """
-#     Remove addresses to particular claim
-#     curl --location --request DELETE 'localhost:7191/drops/claimants' \
-#     --header 'Content-Type: application/json' \
-#     --data-raw '{"dropper_claim_id": UUID, "claimants":[address]}'
-#     """
+    """
+    Add addresses to particular claim
 
-#     try:
-#         results = actions.delete_claimants(
-#             db_session=db_session,
-#             dropper_claim_id=remove_claimants_request.dropper_claim_id,
-#             addresses=remove_claimants_request.addresses,
-#         )
-#     except Exception as e:
-#         raise DropperHTTPException(
-#             status_code=500, detail=f"Error removing claimants {e}"
-#         )
+    curl --location --request POST 'localhost:7191/drops/claimants' \
+    --header 'Content-Type: application/json' \
+    --data-raw '{"dropper_claim_id": UUID, "claimants":[{"address": address, "amount": int}]}'
 
-#     return data.RemoveClaimantsResponse(addresses=results)
+    """
+
+    added_by = "me"  # request.state.user.address read from header in auth middleware
+
+    try:
+        results = actions.add_claimants(
+            db_session=db_session,
+            dropper_claim_id=add_claimants_request.dropper_claim_id,
+            claimants=add_claimants_request.claimants,
+            added_by=added_by,
+        )
+    except Exception as e:
+        raise DropperHTTPException(
+            status_code=500, detail=f"Error adding claimants {e}"
+        )
+
+    return data.ClaimantsResponse(claimants=results)
+
+
+@app.delete("/drops/claimants", response_model=data.RemoveClaimantsResponse)
+async def create_claimants(
+    request: Request,
+    remove_claimants_request: data.DropRemoveClaimantsRequest = Body(...),
+    db_session: Session = Depends(db.yield_db_session),
+) -> data.RemoveClaimantsResponse:
+
+    """
+    Remove addresses to particular claim
+    curl --location --request DELETE 'localhost:7191/drops/claimants' \
+    --header 'Content-Type: application/json' \
+    --data-raw '{"dropper_claim_id": UUID, "claimants":[address]}'
+    """
+
+    try:
+        results = actions.delete_claimants(
+            db_session=db_session,
+            dropper_claim_id=remove_claimants_request.dropper_claim_id,
+            addresses=remove_claimants_request.addresses,
+        )
+    except Exception as e:
+        raise DropperHTTPException(
+            status_code=500, detail=f"Error removing claimants {e}"
+        )
+
+    return data.RemoveClaimantsResponse(addresses=results)
