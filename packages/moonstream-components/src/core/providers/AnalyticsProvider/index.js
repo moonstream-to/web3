71ff0120233bd4861e7f1b1dad4dbd9319d57dbf
Neeraj Kashyap <nkashy1@gmail.com>
2022-04-27 04:15:03 -0700

    Env var gymnastics

--- packages/moonstream-components/src/core/providers/AnalyticsProvider/index.js
+++ packages/moonstream-components/src/core/providers/AnalyticsProvider/index.js
@@ -7,7 +7,7 @@ import UIContext from "../UIProvider/context";
 
 const AnalyticsProvider = ({ children }) => {
   const clientID = useClientID();
-  const analytics = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
+  const analytics = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN_ENGINE;
   const { user, isInit } = useUser();
   const [isMixpanelReady, setIsLoaded] = useState(false);
   const router = useRouter();
