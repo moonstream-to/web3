119ffe3991feeb365c0260a98b70941dc51bb022
Tim Pechersky <tim.pechersky@gmail.com>
2022-05-26 14:14:54 +0200

    better performance, added search claimants API

--- packages/moonstream-components/src/components/ClaimantDetails.js
+++ packages/moonstream-components/src/components/ClaimantDetails.js
@@ -1,50 +1,55 @@
 import React, { useContext } from "react";
-
-import {
-  Flex,
-  Spinner,
-  UnorderedList,
-  ListItem,
-  IconButton,
-  Heading,
-} from "@chakra-ui/react";
+import { Flex, Spinner, IconButton, Heading } from "@chakra-ui/react";
 import Web3Context from "../core/providers/Web3Provider/context";
 import { targetChain } from "../core/providers/Web3Provider";
-import CopyButton from "./CopyButton";
 import { CloseIcon } from "@chakra-ui/icons";
 import { BiTrash } from "react-icons/bi";
-import { useClaim } from "../core/hooks/dropper";
-const ClaimantDetails = ({ claimId, address, onClose, onDeleteClaimant }) => {
+import { useDrop } from "../core/hooks/dropper";
+import useSearch from "../core/hooks/useSearch";
+const ClaimantDetails = ({ claimId, address, onClose }) => {
   const web3ctx = useContext(Web3Context);
 
-  const { signature } = useClaim({
+  const { deleteClaimants } = useDrop({
     targetChain,
     ctx: web3ctx,
     claimId: claimId,
-    claimantAddress: address,
   });
-  if (signature.isLoading) return <Spinner />;
+
+  const { search } = useSearch({
+    pathname: "/drops/claimants/search",
+    query: { address: address, dropper_claim_id: claimId },
+  });
+  if (search.isLoading) return <Spinner />;
   return (
-    <Flex className="ClaimantDetails" direction={"row"}>
-      {signature.data?.signature && (
+    <Flex className="ClaimantDetails" direction={"row"} alignItems="baseline">
+      {search.data?.address && (
         <>
-          <CopyButton text={signature.data.signature}>Signature</CopyButton>
-          <UnorderedList>
-            <ListItem>Deadline: {signature.data.block_deadline}</ListItem>
-            <ListItem>Amount: {signature.data.claim_id}</ListItem>
-          </UnorderedList>
+          <Heading size="sm">Amount: {search.data.amount}</Heading>
           <IconButton
+            size="sm"
             colorScheme="orange"
-            onClick={() => onDeleteClaimant(address)}
+            isLoading={deleteClaimants.isLoading}
+            onClick={() => {
+              deleteClaimants.mutate(
+                { list: [address] },
+                {
+                  onSuccess: () => {
+                    search.remove();
+                    onClose();
+                  },
+                }
+              );
+            }}
             version="ghost"
             icon={<BiTrash />}
           ></IconButton>
         </>
       )}
-      {!signature.data?.signature && <Heading>Not found</Heading>}
+      {!search.data?.address && <Heading size="sm">Not found</Heading>}
       <IconButton
         colorScheme="orange"
         onClick={onClose}
+        size="sm"
         version="ghost"
         icon={<CloseIcon />}
       ></IconButton>
--- packages/moonstream-components/src/components/ClaimersList.js
+++ packages/moonstream-components/src/components/ClaimersList.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { IconButton } from "@chakra-ui/react";
+import React, { useContext, useState } from "react";
+import { IconButton, Spinner } from "@chakra-ui/react";
 import {
   Table,
   Th,
@@ -21,6 +21,10 @@ import { DeleteIcon, TriangleDownIcon } from "@chakra-ui/icons";
 import CopyButton from "./CopyButton";
 import { BiHappyHeartEyes } from "react-icons/bi";
 import ConfirmationRequest from "./ConfirmationRequest";
+import { useDrop } from "../core/hooks/dropper";
+import { targetChain } from "../core/providers/Web3Provider";
+import Web3Context from "../core/providers/Web3Provider/context";
+import Paginator from "./Paginator";
 
 const SORT_BY_TYPES = {
   AMOUNT: 0,
@@ -29,7 +33,21 @@ const SORT_DIRECTION_TYPES = {
   ASC: true,
   DESC: false,
 };
-const ClaimersList = ({ data, onDeleteClaimant }) => {
+const _ClaimersList = ({ dropId }) => {
+  const web3ctx = useContext(Web3Context);
+  const {
+    deleteClaimants,
+    claimants,
+    setClaimantsPage,
+    claimantsPage,
+    setClaimantsPageSize,
+    claimantsPageSize,
+  } = useDrop({
+    targetChain: targetChain,
+    ctx: web3ctx,
+    claimId: dropId,
+  });
+
   const [sortBy, setSortBy] = useState({
     column: SORT_BY_TYPES.AMOUNT,
     direction: SORT_DIRECTION_TYPES.ASC,
@@ -48,150 +66,130 @@ const ClaimersList = ({ data, onDeleteClaimant }) => {
     px: ["2px", "6px", "inherit"],
   };
   return (
-    <Table
-      variant="simple"
-      colorScheme="blue"
-      justifyContent="center"
-      alignItems="baseline"
-      h="auto"
-      size="sm"
+    <Paginator
+      paginatorKey={"claimants"}
+      setPage={setClaimantsPage}
+      setLimit={setClaimantsPageSize}
+      hasMore={claimants.data?.length == claimantsPageSize ? true : false}
+      page={claimantsPage}
+      pageSize={claimantsPageSize}
     >
-      <Thead>
-        <Tr>
-          {/* <Th>
-              <Button
-                variant="link"
-                my={0}
-                mx={0}
-                size={buttonSize}
-                colorScheme={
-                  sortBy.column !== SORT_BY_TYPES.LABEL ? "blue" : "orange"
-                }
-                onClick={() =>
-                  setSortBy({
-                    column: SORT_BY_TYPES.LABEL,
-                    direction:
-                      sortBy.column !== SORT_BY_TYPES.LABEL
-                        ? SORT_DIRECTION_TYPES.ASC
-                        : !sortBy.direction,
-                  })
-                }
-                rightIcon={
-                  <TriangleDownIcon
-                    color={
-                      sortBy.column !== SORT_BY_TYPES.LABEL && "transparent"
-                    }
-                    boxSize="12px"
-                    transform={
-                      sortBy.direction === SORT_DIRECTION_TYPES.ASC
-                        ? "rotate(0deg)"
-                        : "rotate(180deg)"
-                    }
-                  />
-                }
-              >
+      {!claimants.isLoading && (
+        <Table
+          variant="simple"
+          colorScheme="blue"
+          justifyContent="center"
+          alignItems="baseline"
+          h="auto"
+          size="sm"
+        >
+          <Thead>
+            <Tr>
+              <Th {...cellProps} fontSize={["xx-small", "xs", null]}>
                 Address
-              </Button>
-            </Th> */}
-          <Th {...cellProps} fontSize={["xx-small", "xs", null]}>
-            Address
-          </Th>
-          <Th {...cellProps} fontSize={["xx-small", "xs", null]}>
-            <Button
-              mx={0}
-              variant="link"
-              my={0}
-              size={buttonSize}
-              colorScheme={
-                sortBy.column !== SORT_BY_TYPES.AMOUNT ? "blue" : "orange"
-              }
-              onClick={() =>
-                setSortBy({
-                  column: SORT_BY_TYPES.AMOUNT,
-                  direction:
-                    sortBy.column !== SORT_BY_TYPES.AMOUNT
-                      ? SORT_DIRECTION_TYPES.ASC
-                      : !sortBy.direction,
-                })
-              }
-              rightIcon={
-                <TriangleDownIcon
-                  color={
-                    sortBy.column !== SORT_BY_TYPES.AMOUNT && "transparent"
+              </Th>
+              <Th {...cellProps} fontSize={["xx-small", "xs", null]}>
+                <Button
+                  mx={0}
+                  variant="link"
+                  my={0}
+                  size={buttonSize}
+                  colorScheme={
+                    sortBy.column !== SORT_BY_TYPES.AMOUNT ? "blue" : "orange"
                   }
-                  boxSize="12px"
-                  transform={
-                    sortBy.direction === SORT_DIRECTION_TYPES.ASC
-                      ? "rotate(0deg)"
-                      : "rotate(180deg)"
+                  onClick={() =>
+                    setSortBy({
+                      column: SORT_BY_TYPES.AMOUNT,
+                      direction:
+                        sortBy.column !== SORT_BY_TYPES.AMOUNT
+                          ? SORT_DIRECTION_TYPES.ASC
+                          : !sortBy.direction,
+                    })
                   }
-                />
-              }
-            >
-              {`Amount`}
-            </Button>
-          </Th>
-          <Th {...cellProps} fontSize={["xx-small", "xs", null]}>
-            Actions
-          </Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {data?.map((claimant) => {
-          return (
-            <Tr key={`token-row-${claimant.address}`}>
-              <Td
-                mr={4}
-                py={0}
-                {...cellProps}
-                isTruncated
-                maxW={["100px", "150px", "300px"]}
-              >
-                <CopyButton text={claimant.address}>
-                  <code>{claimant.address}</code>
-                </CopyButton>
-              </Td>
-              <Td py={0} {...cellProps}>
-                {claimant.amount}
-              </Td>
-              <Td py={0} {...cellProps}>
-                <ConfirmationRequest
-                  header="Please confirm"
-                  confirmLabel="confirm"
-                  cancelLabel="cancel"
-                  bodyMessage="This will delete claimant form the list"
-                  onConfirm={() => onDeleteClaimant(claimant.address)}
-                >
-                  <IconButton
-                    size="sm"
-                    variant="ghost"
-                    colorScheme="blue"
-                    icon={<DeleteIcon />}
-                  />
-                </ConfirmationRequest>
-
-                <Popover>
-                  <PopoverTrigger>
-                    <IconButton
-                      size="sm"
-                      variant="ghost"
-                      colorScheme="blue"
-                      icon={<BiHappyHeartEyes />}
+                  rightIcon={
+                    <TriangleDownIcon
+                      color={
+                        sortBy.column !== SORT_BY_TYPES.AMOUNT && "transparent"
+                      }
+                      boxSize="12px"
+                      transform={
+                        sortBy.direction === SORT_DIRECTION_TYPES.ASC
+                          ? "rotate(0deg)"
+                          : "rotate(180deg)"
+                      }
                     />
-                  </PopoverTrigger>
-                  <PopoverContent bgColor={"blue.900"}>
-                    <PopoverArrow />
-                    <PopoverCloseButton />
-                    <PopoverHeader>Added by </PopoverHeader>
-                    <PopoverBody>{claimant.added_by}</PopoverBody>
-                  </PopoverContent>
-                </Popover>
-              </Td>
+                  }
+                >
+                  {`Amount`}
+                </Button>
+              </Th>
+              <Th {...cellProps} fontSize={["xx-small", "xs", null]}>
+                Actions
+              </Th>
             </Tr>
-          );
-        })}
-      </Tbody>
-    </Table>
+          </Thead>
+          <Tbody>
+            {claimants.data?.map((claimant) => {
+              return (
+                <Tr key={`token-row-${claimant.address}`}>
+                  <Td
+                    mr={4}
+                    py={0}
+                    {...cellProps}
+                    isTruncated
+                    maxW={["100px", "150px", "300px"]}
+                  >
+                    <CopyButton text={claimant.address}>
+                      <code>{claimant.address}</code>
+                    </CopyButton>
+                  </Td>
+                  <Td py={0} {...cellProps}>
+                    {claimant.amount}
+                  </Td>
+                  <Td py={0} {...cellProps}>
+                    <ConfirmationRequest
+                      header="Please confirm"
+                      confirmLabel="confirm"
+                      cancelLabel="cancel"
+                      bodyMessage="This will delete claimant form the list"
+                      onConfirm={() => {
+                        deleteClaimants.mutate({ list: [claimant.address] });
+                      }}
+                    >
+                      <IconButton
+                        size="sm"
+                        variant="ghost"
+                        colorScheme="blue"
+                        icon={<DeleteIcon />}
+                      />
+                    </ConfirmationRequest>
+
+                    <Popover>
+                      <PopoverTrigger>
+                        <IconButton
+                          size="sm"
+                          variant="ghost"
+                          colorScheme="blue"
+                          icon={<BiHappyHeartEyes />}
+                        />
+                      </PopoverTrigger>
+                      <PopoverContent bgColor={"blue.900"}>
+                        <PopoverArrow />
+                        <PopoverCloseButton />
+                        <PopoverHeader>Added by </PopoverHeader>
+                        <PopoverBody>{claimant.added_by}</PopoverBody>
+                      </PopoverContent>
+                    </Popover>
+                  </Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      )}
+      {claimants.isLoading && <Spinner />}
+    </Paginator>
   );
 };
-export default ClaimersList;
+
+export default React.memo(_ClaimersList);
