5b1b84260c60df8ea5b4e09bdabbe72a1f470973
Andrey Dolgolev <andrey@simiotics.com>
2022-06-13 23:27:37 +0300

    Add cli.

--- engineapi/scripts/fill_raw_amount.py
+++ engineapi/scripts/fill_raw_amount.py
@@ -1,3 +1,5 @@
+import argparse
+
 from brownie import network, Contract
 from typing import Dict, List, Optional, Any
 
@@ -7,112 +9,133 @@ from ..Dropper import Dropper
 from ..MockErc20 import MockErc20
 
 
-# sync raw_amount column with amount column
+def run_fill_raw_amount():
+    # sync raw_amount column with amount column
 
-# create chache of claim token type
-# newtwork contract and list of claims with their token type
+    # create chache of claim token type
+    # newtwork contract and list of claims with their token type
 
-token_tytes: Dict[str, Dict[str, List[Dict[str, Any]]]] = dict()
+    token_tytes: Dict[str, Dict[str, List[Dict[str, Any]]]] = dict()
 
+    with db.yield_db_session_ctx() as db_session:
 
-with db.yield_db_session_ctx() as db_session:
-
-    res = db_session.execute(
-        """select distinct dropper_contracts.blockchain, dropper_contracts.address, dropper_claims.claim_id from dropper_contracts 
-                        left join dropper_claims on dropper_contracts.id = dropper_claims.dropper_contract_id
-                        where dropper_claims.claim_id is not null"""
-    )
-    results = res.fetchall()
-
-    for blockchain, address, claim_id in results:
-        if blockchain not in token_tytes:
-            token_tytes[blockchain] = dict()
-        if address not in token_tytes[blockchain]:
-            token_tytes[blockchain][address] = list()
-        token_tytes[blockchain][address].append(claim_id)
-
-    db_session.execute(
-        """
-        create table temptest
-        (
-            blockchain varchar, 
-            address varchar, 
-            claim_id varchar,
-            token_type varchar,
-            zeros varchar
+        res = db_session.execute(
+            """select distinct dropper_contracts.blockchain, dropper_contracts.address, dropper_claims.claim_id from dropper_contracts 
+                            left join dropper_claims on dropper_contracts.id = dropper_claims.dropper_contract_id
+                            where dropper_claims.claim_id is not null"""
         )
-            
-        """
-    )
+        results = res.fetchall()
 
-    for blockchain in token_tytes:
-        brownie_network = BLOCKCHAINS_TO_BROWNIE_NETWORKS.get(blockchain)
-        network.connect(brownie_network)
-        for address in token_tytes[blockchain]:
-            dropper_contract: Optional[Contract] = Dropper(address)
+        for blockchain, address, claim_id in results:
+            if blockchain not in token_tytes:
+                token_tytes[blockchain] = dict()
+            if address not in token_tytes[blockchain]:
+                token_tytes[blockchain][address] = list()
+            token_tytes[blockchain][address].append(claim_id)
 
-            for claim_id in token_tytes[blockchain][address]:
-                claim_info = dropper_contract.getClaim(claim_id)
-                zeros = None
-                if claim_info[0] == 20:
-                    erc20_contract: Optional[Contract] = MockErc20(claim_info[1])
-                    zeros = "0" * erc20_contract.decimals()
-                db_session.execute(
-                    """
-                            insert into temptest
-                            (
-                                blockchain, 
-                                address, 
-                                claim_id,
-                                token_type,
-                                zeros
+        db_session.execute(
+            """
+            create table temptest
+            (
+                blockchain varchar, 
+                address varchar, 
+                claim_id varchar,
+                token_type varchar,
+                zeros varchar
+            )
+                
+            """
+        )
 
-                            )
-                            values
-                            (
-                                %s, 
-                                %s, 
-                                %s,
-                                %s,
-                                %s
-                            )
-                            """,
-                    (blockchain, address, claim_id, str(claim_info[0]), zeros),
-                )
+        for blockchain in token_tytes:
+            brownie_network = BLOCKCHAINS_TO_BROWNIE_NETWORKS.get(blockchain)
+            network.connect(brownie_network)
+            for address in token_tytes[blockchain]:
+                dropper_contract: Optional[Contract] = Dropper(address)
 
-        network.disconnect()
+                for claim_id in token_tytes[blockchain][address]:
+                    claim_info = dropper_contract.getClaim(claim_id)
+                    zeros = None
+                    if claim_info[0] == 20:
+                        erc20_contract: Optional[Contract] = MockErc20(claim_info[1])
+                        zeros = "0" * erc20_contract.decimals()
+                    db_session.execute(
+                        """
+                                insert into temptest
+                                (
+                                    blockchain, 
+                                    address, 
+                                    claim_id,
+                                    token_type,
+                                    zeros
 
-    # update raw_amount column
-    db_session.execute(
-        """
-        update
-            dropper_claimants
-        set
-            raw_amount = (
-                CASE
-                    WHEN (
-                        select
-                            temptest.token_type
-                        from
-                            temptest
-                            inner join dropper_claims ON temptest.claim_id :: int = dropper_claims.claim_id
-                        where
-                            dropper_claims.id = dropper_claimants.dropper_claim_id
-                    ) :: int = 20 
-                        THEN CONCAT(
-                            CAST(dropper_claimants.amount as varchar),
-                            (
-                                select
-                                    temptest.zeros
-                                from
-                                    temptest
-                                    inner join dropper_claims ON temptest.claim_id :: int = dropper_claims.claim_id
-                                where
-                                    dropper_claims.id = dropper_claimants.dropper_claim_id
-                            )
+                                )
+                                values
+                                (
+                                    %s, 
+                                    %s, 
+                                    %s,
+                                    %s,
+                                    %s
+                                )
+                                """,
+                        (blockchain, address, claim_id, str(claim_info[0]), zeros),
                     )
-                    WHEN true THEN CAST(dropper_claimants.amount as varchar)
-                END
-            );
-        """
+
+            network.disconnect()
+
+        # update raw_amount column
+        db_session.execute(
+            """
+            update
+                dropper_claimants
+            set
+                raw_amount = (
+                    CASE
+                        WHEN (
+                            select
+                                temptest.token_type
+                            from
+                                temptest
+                                inner join dropper_claims ON temptest.claim_id :: int = dropper_claims.claim_id
+                            where
+                                dropper_claims.id = dropper_claimants.dropper_claim_id
+                        ) :: int = 20 
+                            THEN CONCAT(
+                                CAST(dropper_claimants.amount as varchar),
+                                (
+                                    select
+                                        temptest.zeros
+                                    from
+                                        temptest
+                                        inner join dropper_claims ON temptest.claim_id :: int = dropper_claims.claim_id
+                                    where
+                                        dropper_claims.id = dropper_claimants.dropper_claim_id
+                                )
+                        )
+                        WHEN true THEN CAST(dropper_claimants.amount as varchar)
+                    END
+                );
+            """
+        )
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="dao: The command line interface to Moonstream DAO"
     )
+    parser.set_defaults(func=lambda _: parser.print_help())
+    subparsers = parser.add_subparsers()
+
+    run_fill_raw_amount_parser = subparsers.add_parser(
+        "fill_raw_amount", help="Fill raw_amount column"
+    )
+
+    run_fill_raw_amount_parser.set_defaults(func=run_fill_raw_amount)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
