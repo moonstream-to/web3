eca10389782f6b7ee0ecda2d2c92959d9b0fe097
kompotkot <zenitsws@gmail.com>
2022-08-11 11:05:22 +0000

    Tested pkg and rev

--- scm/rev/2022-08-11-alembic_version-R001D1KR4WR2RMUS/default.nix
+++ scm/rev/2022-08-11-alembic_version-R001D1KR4WR2RMUS/default.nix
@@ -1,8 +1,8 @@
 stdargs @ { scm, ... }:
 
 scm.revision {
-    guid = "R001D0FMHJ180QR1";
-    name = "2022-08-10-alembic_version";
+    guid = "R001D1KR4WR2RMUS";
+    name = "2022-08-11-alembic_version";
     upgrade_sql = ./upgrade.sql;
     dependencies = [
         
