a74ad169d8367e0ecc9fce8b53348874aaeaf361
Yhtyyar Sahatov <yhtiyarsahatow@gmail.com>
2022-09-01 00:18:04 +0300

    crafting sdk ready

---
+++ sdk/src/CraftingContract.ts
@@ -0,0 +1,160 @@
+import { ethers } from "ethers"
+import { CraftingFacet__factory } from "./contract-types"
+import { CraftingFacet } from "./contract-types"
+import { PromiseOrValue } from "./contract-types/common"
+import {
+    CraftingInputItemStruct,
+    CraftingOutputItemStruct,
+    RecipeStruct,
+} from "./contract-types/CraftingFacet"
+import {
+    CraftingRecipe,
+    CraftingInputActions,
+    CraftingOutputActions,
+    ERC20Item,
+    ERC1155Item,
+    CraftingItem,
+    CraftingInput,
+    CraftingOutput,
+} from "./types/CraftingItems"
+
+function enumFromNumberValue<T>(
+    enm: { [s: number]: T },
+    value: number
+): T | undefined {
+    return (Object.values(enm) as unknown as number[]).includes(value)
+        ? (value as unknown as T)
+        : undefined
+}
+
+export class CraftingContract {
+    public contract: CraftingFacet
+    public signer?: ethers.Signer = undefined
+    constructor(
+        public address: string,
+        public provider: ethers.providers.Provider
+    ) {
+        this.contract = CraftingFacet__factory.connect(address, provider)
+    }
+
+    public async _getRecipeRaw(recipeId: PromiseOrValue<ethers.BigNumberish>) {
+        return this.contract.getRecipe(recipeId)
+    }
+
+    public async getRecipe(
+        recipeId: PromiseOrValue<ethers.BigNumberish>
+    ): Promise<CraftingRecipe> {
+        let rawRecipe = await this.contract.getRecipe(recipeId)
+        let craftingInputs: Array<CraftingInput> = rawRecipe.inputs.map(
+            (el): CraftingInput => {
+                let action =
+                    el.tokenAction.toNumber() as unknown as CraftingInputActions
+                if (el.tokenType.toString() === "20") {
+                    // let action = CraftingInputActions[
+                    //     el.tokenAction.toNumber()
+                    // ] as keyof typeof CraftingInputActions;
+
+                    return {
+                        action: action,
+                        item: {
+                            tokenAddrress: el.tokenAddress,
+                            amount: el.amount.toString(),
+                        },
+                    }
+                } else {
+                    return {
+                        action: action,
+                        item: {
+                            tokenId: el.tokenId.toString(),
+                            tokenAddrress: el.tokenAddress,
+                            amount: el.amount.toString(),
+                        },
+                    }
+                }
+            }
+        )
+        let craftingOutputs = rawRecipe.outputs.map((el): CraftingOutput => {
+            let action =
+                el.tokenAction.toNumber() as unknown as CraftingOutputActions
+            if (el.tokenType.toString() === "20") {
+                return {
+                    action: action,
+                    item: {
+                        tokenAddrress: el.tokenAddress,
+                        amount: el.amount.toString(),
+                    },
+                }
+            } else {
+                return {
+                    action: action,
+                    item: {
+                        tokenAddrress: el.tokenAddress,
+                        amount: el.amount.toString(),
+                        tokenId: el.tokenId.toString(),
+                    },
+                }
+            }
+        })
+
+        return {
+            isActive: rawRecipe.isActive,
+            craftingInputs: craftingInputs,
+            craftingOutputs: craftingOutputs,
+        }
+    }
+
+    public async numRecipes(): Promise<ethers.BigNumber> {
+        return this.contract.numRecipes()
+    }
+
+    public async craft(
+        recipeId: PromiseOrValue<ethers.BigNumberish>
+    ): Promise<ethers.ContractTransaction> {
+        return this.contract.craft(recipeId)
+    }
+
+    public async addRecipe(
+        recipe: CraftingRecipe,
+        overrides?: ethers.Overrides
+    ): Promise<ethers.BigNumber> {
+        let craftingInputs = recipe.craftingInputs.map(
+            (el): CraftingInputItemStruct => {
+                return {
+                    tokenType: "tokenId" in el.item ? 1155 : 20,
+                    tokenAddress: el.item.tokenAddrress,
+                    tokenId:
+                        "tokenId" in el.item
+                            ? (el.item as ERC1155Item).tokenId
+                            : 0,
+                    amount: el.item.amount,
+                    tokenAction: el.action,
+                }
+            }
+        )
+        let craftingOutputs = recipe.craftingOutputs.map(
+            (el): CraftingOutputItemStruct => {
+                return {
+                    tokenType: "tokenId" in el.item ? 1155 : 20,
+                    tokenAddress: el.item.tokenAddrress,
+                    tokenId:
+                        "tokenId" in el.item
+                            ? (el.item as ERC1155Item).tokenId
+                            : 0,
+                    amount: el.item.amount,
+                    tokenAction: el.action,
+                }
+            }
+        )
+        let recipeStruct: RecipeStruct = {
+            inputs: craftingInputs,
+            outputs: craftingOutputs,
+            isActive: recipe.isActive,
+        }
+        await (await this.contract.addRecipe(recipeStruct, overrides)).wait(1)
+        return this.numRecipes()
+    }
+    public connect(_signer: ethers.Signer) {
+        this.signer = _signer
+        this.contract = this.contract.connect(this.signer)
+    }
+}
---
+++ sdk/src/example.ts
@@ -0,0 +1,116 @@
+import { ethers } from "ethers"
+
+import {
+    CraftingRecipe,
+    CraftingItem,
+    CraftingInputActions,
+    CraftingOutputActions,
+} from "./types/CraftingItems"
+
+import { CraftingContract } from "./CraftingContract"
+
+// function printValues(obj: any) {
+//     console.log("{")
+//     for (var key in obj) {
+//         if (typeof obj[key] === "object") {
+//             printValues(obj[key])
+//         } else {
+//             console.log(`${key}:${obj[key]}`)
+//         }
+//     }
+//     console.log("}")
+// }
+
+async function main() {
+    let rpcUrl = process.env.RPC_URL
+    if (!rpcUrl) {
+        throw new Error("RPC_URL env var is not set")
+    }
+    const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
+
+    let craftingContract = new CraftingContract(
+        "0x8EA6A5EE9B9f7BCa384D804a12180473Ae4BE297",
+        provider
+    )
+
+    let recipesCount = await craftingContract.numRecipes()
+    console.log(`There is ${recipesCount} recipes on this contract`)
+    for (let i = 1; i <= recipesCount.toNumber(); ++i) {
+        console.log(JSON.stringify(await craftingContract.getRecipe(i)))
+        console.log()
+    }
+
+    let privateKey = process.env.PRIVATE_KEY
+    if (!privateKey) {
+        throw new Error("PRIVATE_KEY env var is not set")
+    }
+    const signer = new ethers.Wallet(privateKey, provider)
+    //new ethers.Wallet.fromEncryptedJsonSync()
+    craftingContract.connect(signer)
+
+    let emptyBottle: CraftingItem = {
+        tokenAddrress: "0x0000000000000000000000000000000000000000",
+        amount: "1",
+        tokenId: "1",
+    }
+
+    let milk: CraftingItem = {
+        tokenAddrress: "0x0000000000000000000000000000000000000000",
+        amount: "100000000000000000000", // 100 * 10^18, 100 unim
+    }
+
+    let fullBottle: CraftingItem = {
+        tokenAddrress: "0x0000000000000000000000000000000000000000",
+        amount: "1",
+        tokenId: "2",
+    }
+
+    let bottlingRecipe: CraftingRecipe = {
+        isActive: true,
+        craftingInputs: [
+            {
+                item: emptyBottle,
+                action: CraftingInputActions.BURN,
+            },
+            {
+                item: milk,
+                action: CraftingInputActions.TRANSFER,
+            },
+        ],
+        craftingOutputs: [
+            {
+                item: fullBottle,
+                action: CraftingOutputActions.MINT,
+            },
+        ],
+    }
+    console.log("Adding bottling contract")
+    let recipeId = await craftingContract.addRecipe(bottlingRecipe)
+    console.log(`Bottling recipe is created with id: ${recipeId}`)
+
+    let unbottlingRecipe: CraftingRecipe = {
+        isActive: true,
+        craftingInputs: [
+            {
+                item: fullBottle,
+                action: CraftingInputActions.BURN,
+            },
+        ],
+        craftingOutputs: [
+            {
+                item: emptyBottle,
+                action: CraftingOutputActions.MINT,
+            },
+            {
+                item: milk,
+                action: CraftingOutputActions.TRANSFER,
+            },
+        ],
+    }
+
+    console.log("Adding unbottling contract")
+    let unbottlingRecipeId = await craftingContract.addRecipe(unbottlingRecipe)
+    console.log(`Unbottling recipe is created with id: ${unbottlingRecipeId}`)
+}
+
+main().catch(console.error)
