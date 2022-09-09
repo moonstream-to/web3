1ea557752b25fcdc8583ade5151e9f44f603b376
Neeraj Kashyap <neeraj@bugout.dev>
2022-09-09 13:26:11 -0700

    GOFP test case setup now deploys terminus and payment token

--- cli/enginecli/test_gofp.py
+++ cli/enginecli/test_gofp.py
@@ -2,7 +2,7 @@ import unittest
 
 from brownie import accounts, network
 
-from . import GOFPFacet
+from . import GOFPFacet, MockTerminus, MockErc20
 from .core import gofp_gogogo, ZERO_ADDRESS
 
 
@@ -17,15 +17,39 @@ class GOFPTestCase(unittest.TestCase):
         cls.owner = accounts[0]
         cls.owner_tx_config = {"from": cls.owner}
 
-        cls.deployed_contracts = gofp_gogogo(ZERO_ADDRESS, 0, cls.owner_tx_config)
+        cls.terminus = MockTerminus.MockTerminus(None)
+        cls.terminus.deploy(cls.owner_tx_config)
+
+        cls.payment_token = MockErc20.MockErc20(None)
+        cls.payment_token.deploy("lol", "lol", cls.owner_tx_config)
+
+        cls.terminus.set_payment_token(cls.payment_token.address, cls.owner_tx_config)
+        cls.terminus.set_pool_base_price(1, cls.owner_tx_config)
+
+        cls.payment_token.mint(cls.owner.address, 999999, cls.owner_tx_config)
+
+        cls.payment_token.approve(
+            cls.terminus.address, 2**256 - 1, cls.owner_tx_config
+        )
+
+        cls.terminus.create_pool_v1(1, False, True, cls.owner_tx_config)
+        cls.admin_pool_id = cls.terminus.total_pools()
+
+        cls.terminus.mint(
+            cls.owner.address, cls.admin_pool_id, 1, "", cls.owner_tx_config
+        )
+
+        cls.deployed_contracts = gofp_gogogo(
+            cls.terminus.address, cls.admin_pool_id, cls.owner_tx_config
+        )
         cls.gofp = GOFPFacet.GOFPFacet(cls.deployed_contracts["contracts"]["Diamond"])
 
 
 class TestGOFPDeployment(GOFPTestCase):
     def test_gofp_deployment(self):
         terminus_info = self.gofp.admin_terminus_info()
-        self.assertEqual(terminus_info[0], ZERO_ADDRESS)
-        self.assertEqual(terminus_info[1], 0)
+        self.assertEqual(terminus_info[0], self.terminus.address)
+        self.assertEqual(terminus_info[1], self.admin_pool_id)
 
 
 if __name__ == "__main__":
