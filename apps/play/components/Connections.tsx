54fcf66a672e4b147fa9c7857e9448a53fa885bd
Anton Mushnin <anton.mushnin@gmail.com>
2023-01-17 13:30:49 +0300

    proper connections to future stages

--- apps/play/components/Connections.tsx
+++ apps/play/components/Connections.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Box } from "@chakra-ui/react";
 
 interface ConnectionsProps {
-  links: Map<string, string[]>;
+  links: Map<string, string>;
+  futureStages: { sources: string[]; targets: string[] }[];
+  width: number;
 }
 
 const getPoints = (id: string) => {
@@ -18,109 +20,143 @@ const getPoints = (id: string) => {
   };
 };
 
-const Connections = ({ links }: ConnectionsProps) => {
+const Connections = ({ links, futureStages, width }: ConnectionsProps) => {
   const [connections, setConnections] = useState<React.CSSProperties[]>([]);
 
   useEffect(() => {
-    console.log("Connections useEffect");
     const connections: React.CSSProperties[] = [];
 
-    for (const [key, value] of links.entries()) {
-      const source = getPoints(key);
-      if (!source) {
+    futureStages.forEach(({ sources, targets }) => {
+      const sortedSources = sources
+        .map((targetId) => getPoints(targetId)!)
+        .sort((a, b) => a.top.x - b.top.x);
+      const sortedTargets = targets
+        .map((targetId) => getPoints(targetId)!)
+        .sort((a, b) => a.top.x - b.top.x);
+      const rightSource = sortedSources[sortedSources.length - 1].bottom.x;
+      const rightTarget = sortedTargets[sortedTargets.length - 1].top.x;
+      const leftSource = sortedSources[0].bottom.x;
+      const leftTarget = sortedTargets[0].top.x;
+      const middle =
+        sortedSources[0].bottom.y +
+        (sortedTargets[0].top.y - sortedSources[0].bottom.y) / 2;
+
+      const right = Math.max(rightSource, rightTarget);
+      const left = Math.min(leftSource, leftTarget);
+
+      const box: React.CSSProperties = { border: "dashed 2px white" };
+      box.left = left;
+      box.width = right - left + 1;
+      box.top = left === leftSource ? sortedSources[0].bottom.y : middle;
+      box.height = middle - sortedSources[0].bottom.y;
+      if (leftSource <= leftTarget) {
+        if (leftSource < leftTarget) {
+          box.borderBottomLeftRadius = 10;
+          box.borderBottomRightRadius = 10;
+        } else {
+          box.borderTop = "none";
+        }
+        box.borderTop = "none";
+        sortedTargets.forEach(({ top }) => {
+          connections.push({
+            left: top.x,
+            width: 2,
+            top: middle,
+            height: top.y - middle,
+            borderLeft: box.border,
+          });
+        });
+        for (let i = 1; i < sortedSources.length - 1; i += 1) {
+          connections.push({
+            left: sortedSources[i].bottom.x,
+            width: 2,
+            top: sortedSources[0].bottom.y,
+            height: middle - sortedSources[0].bottom.y,
+            borderLeft: box.border,
+          });
+        }
+      }
+      if (leftSource > leftTarget) {
+        box.borderTopLeftRadius = 10;
+        box.borderTopRightRadius = 10;
+        box.borderBottom = "none";
+
+        sortedSources.forEach(({ bottom }) => {
+          connections.push({
+            left: bottom.x,
+            width: 2,
+            top: bottom.y,
+            height: middle - bottom.y,
+            borderLeft: box.border,
+          });
+        });
+        for (let i = 1; i < sortedTargets.length - 1; i += 1) {
+          connections.push({
+            left: sortedTargets[i].top.x,
+            width: 2,
+            top: middle,
+            height: sortedTargets[0].top.y - middle,
+            borderLeft: box.border,
+          });
+        }
+      }
+      connections.push(box);
+    });
+
+    for (const [sourceId, targetId] of links.entries()) {
+      const source = getPoints(sourceId);
+      const target = getPoints(targetId);
+      if (!source || !target) {
         return;
       }
-      if (value.length > 1) {
-        const borderTop = "dashed 2px white";
-        const leftToRight = value
-          .map((targetId) => getPoints(targetId)!)
-          .sort((a, b) => a.top.x - b.top.x);
-        leftToRight.forEach((target, idx) => {
-          const height = Math.ceil((target.top.y - source.bottom.y) / 2);
-          const middle = source.bottom.y + height;
-          if (target.top.x < source.top.x) {
-            connections.push({
-              top: middle,
-              left: target.top.x,
-              width:
-                Math.min(leftToRight[idx + 1].top.x + 10, source.top.x) -
-                target.top.x,
-              height: target.top.y - middle + 1,
-              borderTop,
-              borderLeft: borderTop,
-              borderRadius: "10px 0 0 0",
-            });
-          } else {
-            connections.push({
-              top: target.top.x === source.top.x ? source.bottom.y : middle,
-              left: source.top.x,
-              width:
-                Math.min(leftToRight[idx].top.x, target.top.x) - source.top.x,
-              height:
-                target.top.x === source.top.x
-                  ? target.top.y - source.bottom.y
-                  : target.top.y - middle + 1,
-              borderTop,
-              borderRight: borderTop,
-              borderRadius: "0 10px 0 0",
-            });
-          }
+
+      const width = Math.abs(target.top.x - source.top.x) / 2;
+      const top = source.bottom.y;
+      const height = Math.ceil((target.top.y - source.bottom.y) / 2);
+      const middle = source.bottom.y + height;
+      const borderLeft = "solid 2px white";
+      if (target.top.x < source.top.x) {
+        connections.push({
+          top,
+          left: source.top.x - width + 1,
+          width,
+          height: height + 1,
+          borderRight: borderLeft,
+          borderBottom: borderLeft,
+          borderRadius: "0 0 10px 0",
+        });
+        connections.push({
+          top: top + height - 1,
+          left: target.top.x,
+          width: width + 1,
+          height: target.top.y - top - height + 1,
+          borderLeft,
+          borderTop: borderLeft,
+          borderRadius: "10px 0 0 0",
         });
       } else {
-        value.forEach((targetId) => {
-          const target = getPoints(targetId);
-          if (!target) {
-            return;
-          }
-          const width = Math.abs(target.top.x - source.top.x) / 2;
-          const top = source.bottom.y;
-          const height = Math.ceil((target.top.y - source.bottom.y) / 2);
-          const middle = source.bottom.y + height;
-          const borderLeft = "solid 2px white";
-          if (target.top.x < source.top.x) {
-            connections.push({
-              top,
-              left: source.top.x - width + 1,
-              width,
-              height: height + 1,
-              borderRight: borderLeft,
-              borderBottom: borderLeft,
-              borderRadius: "0 0 10px 0",
-            });
-            connections.push({
-              top: top + height - 1,
-              left: target.top.x,
-              width: width + 1,
-              height: target.top.y - top - height + 1,
-              borderLeft,
-              borderTop: borderLeft,
-              borderRadius: "10px 0 0 0",
-            });
-          } else {
-            connections.push({
-              top,
-              left: source.top.x,
-              width,
-              height: (target.top.y - top) / 2 + 1,
-              borderLeft,
-              borderBottom: borderLeft,
-              borderRadius: "0 0 0 10px",
-            });
-            connections.push({
-              top: middle - 1,
-              left: source.top.x + width,
-              width,
-              height: target.top.y - middle + 1,
-              borderRight: borderLeft,
-              borderTop: borderLeft,
-              borderRadius: "0 10px 0 0",
-            });
-          }
+        connections.push({
+          top,
+          left: source.top.x,
+          width,
+          height: (target.top.y - top) / 2 + 1,
+          borderLeft,
+          borderBottom: borderLeft,
+          borderRadius: "0 0 0 10px",
+        });
+        connections.push({
+          top: middle - 1,
+          left: source.top.x + width,
+          width,
+          height: target.top.y - middle + 1,
+          borderRight: borderLeft,
+          borderTop: borderLeft,
+          borderRadius: "0 10px 0 0",
         });
       }
     }
     setConnections(connections);
-  }, [links]);
+  }, [links, futureStages, width]);
 
   return (
     <>
--- packages/moonstream-components/src/components/GoFPSessionPanel.tsx
+++ packages/moonstream-components/src/components/GoFPSessionPanel.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Flex, Text } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Center, Flex, Text } from "@chakra-ui/react";
 import { SessionMetadata } from "./GoFPTypes";
 import StagePanel from "./GoFPStagePanel";
 import Connections from "../../../../apps/play/components/Connections";
@@ -12,25 +12,53 @@ const SessionPanel = ({
   sessionMetadata: SessionMetadata;
   onDrop: (item: any) => void;
 }) => {
-  const [links, setLinks] = useState(new Map<string, string[]>());
-  const handleResize = () => {
-    const newLinks = new Map<string, string[]>();
-    newLinks.set("card-0-1", ["card-1-0"]);
-    newLinks.set("card-1-0", ["card-2-3"]);
-    newLinks.set("card-2-3", ["card-3-1"]);
-    newLinks.set("card-3-1", ["card-4-1"]);
-    newLinks.set("card-4-1", ["card-5-1", "card-5-0", "card-5-2", "card-5-3"]);
-    newLinks.set("card-5-1", ["card-6-1", "card-6-0", "card-6-2"]);
-    newLinks.set("card-6-2", ["card-7-1", "card-7-0", "card-7-2"]);
-    newLinks.set("card-7-0", ["card-8-1", "card-8-0", "card-8-2", "card-8-3"]);
+  const [currentStage, setCurrentStage] = useState(4);
+  const [links, setLinks] = useState(new Map<string, string>());
+  const [futureStages, setFutureStages] = useState<
+    { sources: string[]; targets: string[] }[]
+  >([]);
+  const [containerWidth, setContainerWidth] = useState(0);
 
+  useEffect(() => {
+    const newLinks = new Map<string, string>();
+    newLinks.set("card-0-1", "card-1-0");
+    newLinks.set("card-1-0", "card-2-3");
+    newLinks.set("card-2-3", "card-3-1");
+    newLinks.set("card-3-1", "card-4-1");
     setLinks(newLinks);
+
+    const extendedStages = sessionMetadata.stages
+      .concat(sessionMetadata.stages)
+      .concat(sessionMetadata.stages);
+    const newFutureStages: { sources: string[]; targets: string[] }[] = [];
+    for (let i = currentStage; i < extendedStages.length - 1; i += 1) {
+      const futureStage: { sources: string[]; targets: string[] } = {
+        sources: [],
+        targets: [],
+      };
+      for (let j = 0; j < extendedStages[i].paths.length; j += 1) {
+        futureStage.sources.push(`card-${i}-${[j]}`);
+      }
+      for (let j = 0; j < extendedStages[i + 1].paths.length; j += 1) {
+        futureStage.targets.push(`card-${i + 1}-${[j]}`);
+      }
+      newFutureStages.push(futureStage);
+    }
+    setFutureStages(newFutureStages);
+  }, [sessionMetadata, currentStage]);
+
+  const handleResize = ({ width }: { width: number }) => {
+    setContainerWidth(width);
   };
 
   return (
     <ResizeObserver onResize={handleResize}>
       <Flex flexDirection="column" position="relative">
-        <Connections links={links} />
+        <Connections
+          links={links}
+          futureStages={futureStages}
+          width={containerWidth}
+        />
         <Text fontSize="lg" pb={20}>
           {sessionMetadata.lore}
         </Text>
@@ -39,12 +67,13 @@ const SessionPanel = ({
           .concat(sessionMetadata.stages)
           .map((stage, idx) => {
             return (
-              <StagePanel
-                key={idx}
-                stageMetadata={stage}
-                onDrop={onDrop}
-                id={idx}
-              ></StagePanel>
+              <Center key={idx}>
+                <StagePanel
+                  stageMetadata={stage}
+                  onDrop={onDrop}
+                  id={idx}
+                ></StagePanel>
+              </Center>
             );
           })}
       </Flex>
