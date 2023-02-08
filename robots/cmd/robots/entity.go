3c11bf5b252d4a65f08b61cf92ef1dd1a199c039
kompotkot <sumarokovsergei@yandex.ru>
2023-02-08 12:59:41 +0000

    Initial functionality of robots for airdrop

---
+++ robots/cmd/robots/entity.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+type EntityResponse struct {
+	EntityId       string              `json:"entity_id"`
+	CollectionId   string              `json:"collection_id"`
+	Address        string              `json:"address"`
+	Blockchain     string              `json:"blockchain"`
+	Name           string              `json:"name"`
+	RequiredFields []map[string]string `json:"required_fields"`
+	CreatedAt      string              `json:"created_at"`
+	UpdatedAt      string              `json:"updated_at"`
+}
+
+type EntitySearchResponse struct {
+	TotalResults int64            `json:"total_results"`
+	Offset       int64            `json:"offset"`
+	NextOffset   int64            `json:"next_offset"`
+	MaxScore     float64          `json:"max_score"`
+	Entities     []EntityResponse `json:"entities"`
+}
+
+type EntityClient struct {
+	PublicEndpoint string
+	CollectionId   string
+
+	Headers map[string]string
+}
+
+func (ec *EntityClient) InitializeEntityClient(collection_id string) error {
+	MOONSTREAM_ENTITY_URI := os.Getenv("MOONSTREAM_ENTITY_URI")
+	if MOONSTREAM_ENTITY_URI == "" {
+		return errors.New("Environment variable MOONSTREAM_ENTITY_URI should be specified")
+	}
+
+	ec.PublicEndpoint = fmt.Sprintf("%s/public", MOONSTREAM_ENTITY_URI)
+	ec.CollectionId = collection_id
+
+	if ec.Headers == nil {
+		ec.Headers = make(map[string]string)
+	}
+	ec.Headers["X-Moonstream-Robots"] = "airdrop-robot"
+
+	return nil
+}
+
+// Make HTTP calls to required servers
+func caller(method, url string, reqBody io.Reader, headers map[string]string, timeout int64) (*[]byte, int, error) {
+	req, err := http.NewRequest(method, url, reqBody)
+	if err != nil {
+		return nil, 0, err
+	}
+	if len(headers) > 0 {
+		for k, v := range headers {
+			req.Header.Set(k, v)
+		}
+	}
+
+	client := http.Client{Timeout: time.Second * time.Duration(timeout)}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	defer resp.Body.Close()
+
+	// Parse response
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, resp.StatusCode, err
+	}
+
+	return &body, resp.StatusCode, nil
+}
+
+// FetchPublicSearchUntouched request not touched entities, ready to airdrop
+// TODO(kompotkot): Pass with robots header unique identifier of robot
+func (ec *EntityClient) FetchPublicSearchUntouched(limit, timeout int64) (int, EntitySearchResponse, error) {
+	data := EntitySearchResponse{}
+
+	url := fmt.Sprintf("%s/collections/%s/search?required_field=!touch:true&limit=%d", ec.PublicEndpoint, ec.CollectionId, limit)
+	// url := fmt.Sprintf("%s/collections/%s/search?limit=%d", ec.PublicEndpoint, ec.CollectionId, limit)
+	body, status_code, err := caller("GET", url, nil, ec.Headers, timeout)
+	if err != nil {
+		return status_code, data, err
+	}
+
+	var resp EntitySearchResponse
+	err = json.Unmarshal(*body, &resp)
+	if err != nil {
+		return status_code, data, err
+	}
+	data = resp
+
+	return status_code, data, nil
+}
+
+// TODO(kompotkot): Create batch endpoint for tags creation
+func (ec *EntityClient) TouchPublicEntity(entityId string, timeout int64) (int, []string, error) {
+	var data []string
+
+	url := fmt.Sprintf("%s/collections/%s/entities/%s", ec.PublicEndpoint, ec.CollectionId, entityId)
+	body, status_code, err := caller("PUT", url, nil, ec.Headers, timeout)
+	if err != nil {
+		return status_code, data, err
+	}
+
+	var resp []string
+	err = json.Unmarshal(*body, &resp)
+	if err != nil {
+		return status_code, data, err
+	}
+	data = resp
+
+	return status_code, data, nil
+}
---
+++ robots/cmd/robots/operations.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"log"
+	"math/big"
+)
+
+type Claimant struct {
+	EntityId string
+	Address  string
+}
+
+func AirdropRun(entity_client EntityClient, pool_id int64, contract ContractTerminus, signer Signer, network Network, value int64) (int64, error) {
+	status_code, search_data, err := entity_client.FetchPublicSearchUntouched(10, 10)
+	if err != nil {
+		return 0, err
+	}
+	log.Printf("Received response %d from entities API with %d results", status_code, search_data.TotalResults)
+
+	var claimants_len int64
+	var claimants []Claimant
+	for _, entity := range search_data.Entities {
+		claimants = append(claimants, Claimant{
+			EntityId: entity.EntityId,
+			Address:  entity.Address,
+		})
+		claimants_len++
+	}
+
+	if claimants_len == 0 {
+		return claimants_len, nil
+	}
+
+	// Fetch balances for addresses and update list
+	balances, err := contract.BalanceOfBatch(nil, claimants, pool_id)
+	if err != nil {
+		return 0, err
+	}
+
+	zeroBigInt := big.NewInt(0)
+	var empty_claimants_len int64
+	var empty_claimants []Claimant
+	for i, balance := range balances {
+		if balance.Cmp(zeroBigInt) == 0 {
+			empty_claimants = append(empty_claimants, claimants[i])
+			empty_claimants_len++
+		}
+	}
+
+	if empty_claimants_len > 0 {
+		log.Printf("Ready to send tokens for %d addresses", empty_claimants_len)
+
+		auth, err := signer.CreateTransactor(network)
+		if err != nil {
+			return empty_claimants_len, err
+		}
+
+		tx, err := contract.PoolMintBatch(auth, pool_id, empty_claimants, value)
+		if err != nil {
+			return empty_claimants_len, err
+		}
+		log.Printf("Pending tx for PoolMintBatch: 0x%x", tx.Hash())
+	}
+
+	var touched_entities int64
+	for _, claimant := range claimants {
+		_, _, err := entity_client.TouchPublicEntity(claimant.EntityId, 10)
+		if err != nil {
+			log.Printf("Unable to touch entity with ID: %s for claimant: %s, err: %v", claimant.EntityId, claimant.Address, err)
+			continue
+		}
+		touched_entities++
+	}
+	log.Printf("Marked %d entities from %d claimants total", touched_entities, claimants_len)
+
+	return empty_claimants_len, nil
+}
